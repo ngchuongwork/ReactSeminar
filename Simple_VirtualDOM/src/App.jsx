1080b4749e162d01840d15d2c8b4d4e37f31a02a
chuong-nguyen <ngchuongwork@gmail.com>
2024-12-26 23:30:34 +0700

    exaple with dom.html

--- Simple_VirtualDOM/src/App.jsx
+++ Simple_VirtualDOM/src/App.jsx
@@ -5,32 +5,32 @@ import './App.css'
 
 
 //example with DOM.html
-  // function App() {
-  //   const [text, setText] = useState("Hello");
+  function App() {
+    const [text, setText] = useState("Hello");
 
-  //   return (
-  //     <div>
-  //       <h1>{text}</h1>
-  //       <button name="btn1" onClick={() => {setText("Hi");console.log("Click button")}}>Change Text</button>
-  //     </div>
-  //   );
-  // }
+    return (
+      <div>
+        <h1>{text}</h1>
+        <button name="btn1" onClick={() => {setText("Hi");console.log("Click button")}}>Change Text</button>
+      </div>
+    );
+  }
 
 //example how virtual DOM work
-function App() {
-  const [text, setText] = useState("Hello");
-  const [count, setCount] = useState(0);
+// function App() {
+//   const [text, setText] = useState("Hello");
+//   const [count, setCount] = useState(0);
 
-  return (
-    <div>
-      <h1>{text}</h1>
-      Count:<h2>{count}</h2>
-      <button name="btn1" onClick={() => setText("Hi")}>Change Text</button>
-      <button name="btn2" onClick={() => {setCount(count => count + 1)}}>Count</button>
+//   return (
+//     <div>
+//       <h1>{text}</h1>
+//       Count:<h2>{count}</h2>
+//       <button name="btn1" onClick={() => setText("Hi")}>Change Text</button>
+//       <button name="btn2" onClick={() => {setCount(count => count + 1)}}>Count</button>
 
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
 
 
 
