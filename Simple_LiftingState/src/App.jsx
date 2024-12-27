72512a3c205cf034c1271c4f0a0a9fccd6c6f271
nhat-chuong <nhat-chuong@system-exe.com.vn>
2024-12-27 11:18:26 +0700

    add code lifting state

---
+++ Simple_LiftingState/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Accordion from './Accordion'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Accordion></Accordion>
+    </>
+  )
+}
+
+export default App
